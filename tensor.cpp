97c311e449e20ed668ef9bad96c4dd3e3be46d98
unshun <unshun131@gmail.com>
2026-09-07 15:03:45 +0800

    update

--- tensor.cpp
+++ tensor.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-// Constructor
+// Constructor : initialize
 Tensor::Tensor(const vector<size_t>& input_shape) {
     // shape
     this->shape_ = input_shape;
@@ -23,25 +23,29 @@ Tensor::Tensor(const vector<size_t>& input_shape) {
     this->data_ = elements;
 }
 
-// numel
+// numel : return data size
 size_t Tensor::numel() const {
     return this->data_.size();
 }
 
+// shape() : return shape
 const vector<size_t>& Tensor::shape() const {
     return this->shape_;    
 }
 
+// fill : fill the vlaue to all the data in data_
 void Tensor::fill(float value) {
     for(size_t i = 0; i < this->data_.size(); i++) {
         this->data_[i] = value;
     }
 }
 
+// at : return data[index]
 float Tensor::at(size_t index) const {
     return this->data_.at(index);
 }
 
+// set : set value to data[idnex]
 void Tensor::set(size_t index, float value) {
     this->data_.at(index) = value;
 }
