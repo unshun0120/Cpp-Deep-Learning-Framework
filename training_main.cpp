c4698fc0875210c9270e30ff9582bdcc7a93144c
unshun <unshun131@gmail.com>
2026-09-11 12:16:56 +0800

    training loop

--- training_main.cpp
+++ training_main.cpp
@@ -13,23 +13,28 @@ int main() {
     weight.set(0, 1); 
     bias.set(0, 0);
     target.set(0, 3);   target.set(1, 5);
-
-    Tensor prediction = linear(input, weight, bias);
-    float loss = mse_loss(prediction, target);
-    cout << "fisr loss" << loss << endl;
-    Tensor grad_output = mse_loss_backward(prediction, target);
-    // Tensor backward_relu = relu_backward(input, grad_output);
-    // Tensor backward_input = linear_backward_input(grad_output, backward_relu);
-    Tensor backward_weight = linear_backward_weight(input, grad_output);
-    Tensor backward_bias = linear_backward_bias(grad_output);        
     
+    size_t epoch = 100;
     float learning_rate = 0.1;
-    sgd_step(weight, backward_weight, learning_rate);
-    sgd_step(bias, backward_bias, learning_rate);
     
-    prediction = linear(input, weight, bias);
-    loss = mse_loss(prediction, target);
-    cout << "second loss" << loss << endl;
+    for(size_t i = 0; i < epoch; i++) {
+        Tensor prediction = linear(input, weight, bias);
+        float loss = mse_loss(prediction, target);
+        
+        if(i % 10 == 0) {
+            cout << "epoch : " << i << " loss: " << loss  << " weight: " << weight.at(0) << " bias: " << bias.at(0) << endl;
+        }
+
+        Tensor grad_output = mse_loss_backward(prediction, target);
+        // Tensor backward_relu = relu_backward(input, grad_output);
+        // Tensor backward_input = linear_backward_input(grad_output, backward_relu);
+        Tensor backward_weight = linear_backward_weight(input, grad_output);
+        Tensor backward_bias = linear_backward_bias(grad_output);        
+        
+        sgd_step(weight, backward_weight, learning_rate);
+        sgd_step(bias, backward_bias, learning_rate);
+        
+    }
 
     return 0;
 }
